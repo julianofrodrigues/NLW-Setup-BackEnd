dbe213663a0e142abb8cc33b79962460f00fb93d
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2023-01-27 15:50:07 -0300

    alter config server

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import cors from '@fastify/cors';
 import { appRoutes } from "./routes";
 
 const app = Fastify();
-app. register(cors);
+app. register(cors, {origin: true});
 app.register(appRoutes)
 
 
 app.listen({
-    port: 3333
-}).then( () => {
-    console.log('server run')
+    port: 3333,
+    host: '0.0.0.0'
+}).then( (url) => {
+    console.log(`server run on ${url}`)
 })
